8e99dd99273dcc956e41e0655a7739b194eddd71
Rokas <rookas.rudzenskas@gmail.com>
2021-06-23 17:23:36 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import {auth, provider} from "./firebase";
+
 
 function App() {
+
+
   return (
     <div className="app">
-      <h1>Hello 🚀</h1>
+      <h1>Hello world! 🚀, Let's build the firebase login 🤟</h1>
     </div>
   );
 }

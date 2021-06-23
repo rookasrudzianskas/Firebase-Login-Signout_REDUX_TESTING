fa280714210dcef814860fb2cf15dc22477ae113
Rokas <rookas.rudzenskas@gmail.com>
2021-06-23 17:26:23 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import {auth, provider} from "./firebase";
+import {useDispatch, useSelector} from "react-redux";
+import {selectUserEmail, selectUserName} from "./features/userSlice";
 
 
 function App() {
 
+    const dispatch = useDispatch();
+    const userName = useSelector(selectUserName);
+    const userEmail = useSelector(selectUserEmail);
 
   return (
     <div className="app">
